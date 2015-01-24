6b7df0444466ac4fef619eba619cafe9a2dfbe06
2063602t <2063602t@student.gla.ac.uk>
2015-01-24 19:55:31 +0000

    stopped for the night; completed twd 7.3.2

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
 # Register your models here.
-admin.site.register(model_or_iterable=Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(model_or_iterable=Category, admin_class=CategoryAdmin)
 
 
 class PageAdmin(admin.ModelAdmin):
     fields = ["category", "title", "url", "views"]
     list_display = ("title", "category", "url")
     list_filter = ['category']
-
 
 admin.site.register(model_or_iterable=Page, admin_class=PageAdmin)
